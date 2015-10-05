5012064ce1637dffa2793ea87761892453e70724
shutty <grebennikov.roman@gmail.com>
2015-10-05 17:59:07 +0300

    protocol: add correct version detection for frames

--- src/main/scala/com/github/romangrebennikov/columnize/protocol/Frame.scala
+++ src/main/scala/com/github/romangrebennikov/columnize/protocol/Frame.scala
@@ -5,27 +5,30 @@ import java.nio.ByteBuffer
 /**
  * Created by shutty on 10/5/15.
  */
-case class Frame(direction:Frame.Direction, version:Byte, flags:Flags, stream:Byte, opcode:Byte, length:Int, body:ByteBuffer) {
+case class Frame(direction:Frame.Direction, version:Byte, flags:Flags, stream:Short, opcode:Byte, length:Int, body:ByteBuffer) {
 }
 
 object Frame {
   sealed trait Direction
   case object Request extends Direction
   case object Response extends Direction
+  val RESPONSE_FLAG = 0x80.toByte
   def apply(raw:ByteBuffer) = {
-    val header = raw.get() match {
-      case 4 => (Request, 4.toByte)
-      case 84 => (Response, 4.toByte)
-      case _ => throw new IllegalArgumentException("unsupported version")
+    val buffer = raw.slice()
+    val header = buffer.get()
+    val direction = (header & RESPONSE_FLAG) == RESPONSE_FLAG match {
+      case true => Response
+      case false => Request
     }
+    val version = (header & ~RESPONSE_FLAG).toByte
     new Frame(
-      direction = header._1,
-      version = header._2,
-      flags = Flags(raw.get()),
-      stream = raw.get(),
-      opcode = raw.get(),
-      length = raw.getInt,
-      body = raw.slice()
+      direction = direction,
+      version = version,
+      flags = Flags(buffer.get()),
+      stream = buffer.getShort,
+      opcode = buffer.get(),
+      length = buffer.getInt,
+      body = buffer.slice()
     )
   }
 }
---
+++ src/test/scala/protocol/FrameSpec.scala
@@ -0,0 +1,28 @@
+package protocol
+
+import java.nio.ByteBuffer
+
+import com.github.romangrebennikov.columnize.protocol.{Flags, Frame}
+import org.scalatest.{MustMatchers, WordSpecLike}
+
+/**
+ * Created by shutty on 10/5/15.
+ */
+class FrameSpec extends WordSpecLike with MustMatchers {
+  "Frame" must {
+    "not spoil original bytebuffer position" in {
+      val hello = ByteBuffer.wrap(Array(4,0,0,0,0,0,0,0,0).map(_.toByte))
+      val frame = Frame(hello)
+      assert(hello.position() == 0)
+    }
+    "decode frame length" in {
+      val len = ByteBuffer.wrap(Array(4,0,0,0,0,0,0,0,0).map(_.toByte))
+      assert(Frame(len) == Frame(Frame.Request, 4, Flags.empty, 0, 0, 0, ByteBuffer.allocate(0)))
+    }
+    "allow large stream id" in {
+      val buf = ByteBuffer.wrap(Array(4,0,127,127,0,0,0,0,0).map(_.toByte))
+      assert(Frame(buf).stream > 127)
+    }
+  }
+
+}
